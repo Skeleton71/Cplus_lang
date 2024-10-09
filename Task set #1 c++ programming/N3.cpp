84136d0a795042b6ba59e06aa86095c388cdaf66
Korshakov Aleksandr <1>
2024-10-09 19:57:39 +0300

    init

---
+++ Task set #1 c++ programming/N3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+  int m1, m2, R;
+  float F;
+ 
+  cin >> m1 >> m2 >> R;
+  F = 6.673 * pow(10, -8) * m1 * m2 / (R * R);
+  cout << F;
+  return 0;
+} 
