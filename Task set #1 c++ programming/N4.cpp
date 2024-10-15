f72cdc8a9517b1e8e46b24d83405271df13e8e4a
Skeleton71 <pushkin9000@gmail.com>
2024-10-15 11:25:22 +0300

    N4.cpp

--- Task set #1 c++ programming/N4.cpp
+++ Task set #1 c++ programming/N4.cpp
@@ -3,12 +3,11 @@
 using namespace std;
 
 int main()
-{ double x1,x2,y1,y2,d;
-cout<<"X1 AND X2"<<endl;
-cin>>x1>>x2;
-cout<<"Y1 AND Y2"<<endl;
-cin>>y1>>y2;
-d=sqrt(pow(x2 - x1,2) + pow(y2 - y1,2));
-cout<<"D="<<d;
+{ 
+    double x1,x2,y1,y2,d;
+    cin>>x1>>x2;
+    cin>>y1>>y2;
+    d = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    cout<<"D="<<d;
     return 0;
 }
